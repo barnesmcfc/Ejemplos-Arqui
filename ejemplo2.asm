0246f04b24c41f47238f04b9b795f109850a60a0
barnesmcfc <rebp96@gmail.com>
2016-09-29 11:02:27 -0600

    Actualización código Final

--- ejemplo2.asm
+++ ejemplo2.asm
@@ -1,15 +1,16 @@
-stout equ 1     			;Cada vez que salga stdout pone 1
+stdout equ 1     			;Cada vez que salga stdout pone 1
 sys_write equ 4  			;Cada vez que salga sys_write pone 4
+sys_exit equ 1
 
 section .bss				;Segmento que se pide a la memoria no inicializada
 	resultado resb 16		;Campo para 16 ASCII, reserva 16 bytes. Se reserva una cantidad considerable porque ah[i debe caber el n[umero al imprimirlo
 
 section .data				;Memoria inicialiada
-	msj: db "El numero es:"	;Declare Byte (13 bytes)
-	len equ $-msj			;$ se coloca en la siguiente posición 
+	msj: db "El numero es: ";Declare Byte (13 bytes)
+	len: equ $-msj			;$ se coloca en la siguiente posición 
 	msj2: db ".", 0xA		
 	len2: equ $-msj2
-	numero: dd 777001		;8 dígitos en hexadecimal. Double es espacio en memoria de 32-bits
+	numero: dd 0b111111		;Máximo 8 dígitos en hexadecimal. Double es espacio en memoria de 32-bits, el numero que quiera
 
 section .text				
 	global _start			
@@ -20,7 +21,7 @@ imprime_mensaje:
 	mov ecx, msj			;ecx puntero a lo que se quiere imprimir
 	mov edx, len			;Cantidad de caracteres a imprimir
 	mov eax, sys_write		;Servicio 4 del SO, escribe en la salida		
-	mov ebx, stout			;Salida estandar
+	mov ebx, stdout			;Salida estandar
 	int 80h					;Invoca el servicio que pide los datos correspondientes a cada registro
 
 divisiones_sucesivas:		
@@ -37,29 +38,26 @@ divisiones_sucesivas:
 	or dl, 30h				;Se hace or porque los primeros 4-bits de dl son siempre 0
 	mov [ebx], byte dl		;pone el caracter en resltado
 	dec ebx					;decrementar
-	test eax				;Resta eax-eax y actualiza las banderas
+	test eax,eax			;Resta eax-eax y actualiza las banderas
 	jnz .division 			;Salta si la bandera Z está en 1
 
-;------------------------------------------------------------------------------------------------
-
-
-Displaytext:
-	mov eax,4
-	mov ebx,1
-	int 80h					;se revisa lo que hay en eax
-	ret
 imprime_numero:
-	mov edx, resultado +15 	;edx : largo
-	sub edx,ebx				; substrae ebx-edx
-	mov ecx, ebx			
-	inc ecx					;puntero a "El mensaje"
-	call Displaytext
+	mov edx, resultado+15	
+	sub edx, ebx			;Los resto para obtener el largo del mensaje por posiciones
+	mov ecx,ebx				;Puntero a lo que voy a imprimir
+	inc ecx					;Incrementa puntero
+	mov eax, sys_write		;4
+	mov ebx,stdout			;1
+	int 80h
+	
+imprimir_punto:
+	mov edx, len2			
+	mov ecx, msj2
+	mov eax,sys_write
+	mov ebx, stdout
+	int 80h
 
-imprime_punto:
-	mov ecx, msg2
-	mov edx, len2
-	call Displaytext
-Fin:
-	mov eax,1
-	xor edx,edx
+fin:						;Termina el programa
+	mov eax, sys_exit
+	xor ebx,ebx				;Pone en 0 para indicar que no hay error
 	int 80h
