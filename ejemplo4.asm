f2d08fcf5d4085222bd6efba13937c243728163e
barnesmcfc <rebp96@gmail.com>
2016-10-25 11:36:45 -0600

    Subrutinas

--- ejemplo4.asm
+++ ejemplo4.asm
@@ -1,4 +1,11 @@
 ;Programa que recibe el nombre de usuario y lo muestra en pantalla
+sys_exit equ 1   	;Finaliza el programa
+sys_read equ 3   	;Lee y almacena de un archivo
+sys_write equ 4		;Escribe 
+sys_nanosleep equ 162 	;Pausa
+stdin equ 0      	;Entrada estándar
+stdout equ 1     	;Salida estándar
+
 
 section .data
 	msg: db "Cual es su nombre?",10				;mensaje a imprimir
@@ -25,32 +32,45 @@ lee_nombre:
 	mov ecx, lp_buffer						;Donde queda el arreglo de bytes
 	mov edx, buf_len						;Len
 	mov ebx, 0								;La entrada 0 es el teclado
-	mov eax, 3									;read
+	mov eax, sys_read								;read
 	int 80h
 	push eax								;Read pone en EAX la cantidad de butes leídos al terminar
 
 despliega_mensaje2:
 	mov ecx, msg2
 	mov edx, len2
-	call Displaytext
+	call print
 	
 wait_sec:									;Pausa antes de desplegar mensaje final
 	mov ebx, tiempo							;Cantidad de segundos
 	xor ecx,ecx 							;lo pone en 0
-	mov eax, 162
+	mov eax, sys_nanosleep
 	int 80h
 	
 muestra_nombre:
 	mov ecx, lp_buffer						;Puntero
 	pop edx									;Se había guardado el largo del nombre
-	call Displaytext
+	call print
 Fin:
-	mov eax,1
+	mov eax,sys_exit
 	xor ebx,ebx
 	int 80h
+;-------------------------------------------------------------------------------------------------------------------------
+;--------------------------------------SUBRUTINAS-------------------------------------------------------------------------
+read:	;Entrada: ECX: Puntero adonde quiero guardar el mensaje / EDX: Largo de memoria reservada
+	mov eax, sys_read
+	mov ebx, 0								;La entrada 0 es el teclado
+	int 80h
+	ret
 
-Displaytext:
-	mov eax,4
-	mov ebx,1
-	int 80h		;se revisa lo que hay en eax
+print:	;Entrada: ECX: puntero a lo que se imprime / EDX: Largo
+	mov eax,sys_write
+	mov ebx,stdout
+	int 80h	
+	ret
+
+pausa:	;Entrada puntero a la estructura en EVX
+	xor ecx,ecx 			;lo pone en 0
+	mov eax, sys_nanosleep
+	int 80h
 	ret
